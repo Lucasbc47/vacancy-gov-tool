d94eb641205e7521028debaa1ff4549aa6e46f28
Lucas <122161095+Lucasbc47@users.noreply.github.com>
2023-09-15 08:50:08 -0300

    Update example.py

--- example.py
+++ example.py
@@ -6,8 +6,11 @@ UF_RA = ""
 DATA_DE_NASCIMENTO = "00/00/0000" 
 
 
-def salvar(resultado: dict, arquivo: str):
-    with open(arquivo, 'w') as file:
+def salvar(resultado: dict, nome_arquivo: str) -> None:
+    """
+    Salva um arquivo TXT em base nos resultados adquiridos
+    """
+    with open(f'{nome_arquivo}.txt', 'w') as file:
         for key, value in resultado.items():
             file.write(f"{key}: {value}\n")
 
@@ -19,7 +22,7 @@ def main():
         dtNasc=DATA_DE_NASCIMENTO
     )
 
-    salvar(resultado, "resultados.txt")
+    salvar(resultado, "resultados")
     print("Consulta feita! Abra o arquivo \"resultados.txt\".")
 
 if __name__ == "__main__":
